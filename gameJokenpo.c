cb6a9168f4939d25970f4b237bf3368eee7c7a29
SilvaVictorDev <silva_victor@id.uff.br>
2025-09-15 08:01:22 -0300

    Novos arquivos

---
+++ gameJokenpo.c
@@ -0,0 +1,56 @@
+// Este Programa implementa a lógica do jogo Jokenpo usando o switch e o ifeelse.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    int escolhaJogador, escolhaComputador;
+    srand(time(0));
+
+    printf("### Game Jokenpô ###\n");
+    printf("Escolha uma opção:\n");
+    printf("1. Pedra\n");
+    printf("2. Papel\n");
+    printf("3. Tesoura\n");
+    printf("Escolha: ");
+    scanf("%d", &escolhaJogador);
+    
+    
+    escolhaComputador = rand() % 3 + 1;
+
+    switch (escolhaJogador)
+    {
+    case 1:
+        printf("Jogador: Pedra - ");
+        break;
+    case 2:
+        printf("Jogador: Papel - ");
+        break;
+    case 3:
+        printf("Jogador: Pedra - ");
+        break;
+    default:
+        break;
+    }
+    
+    switch (escolhaComputador)
+    {
+    case 1:
+        printf("Computador: Pedra\n");
+        break;
+    case 2:
+        printf("Computador: Papel\n");
+        break;
+    case 3:
+        printf("Computador: Pedra\n");
+        break;
+    }
+    
+    
+    
+    
+    
+    return 0;
+}
