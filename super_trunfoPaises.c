13eb26871662b37801e8afe02984481a91480b29
SilvaVictorDev <silva_victor@id.uff.br>
2025-09-15 18:41:37 -0300

    update

--- super_trunfoPaises.c
+++ super_trunfoPaises.c
@@ -11,7 +11,8 @@ int main(){
     // Variables declaration
 
     int population1, turistcAttractions1,
-        population2, turistcAttractions2;
+        population2, turistcAttractions2,
+        option;
     float area1, PIB1, densPop1, pibPerCap1, supPower1, invDensPop1,
           area2, PIB2, densPop2, pibPerCap2, supPower2, invDensPop2;
     char country1[50], country2[50];
@@ -46,9 +47,7 @@ int main(){
     printf("Digite o nome do País: ");
     fgets(country2, sizeof(country2), stdin);
 
-    printf("Digite a população: ");
-    scanf("%d", &population2);
-    getchar();
+    
 
     printf("Digite a área: ");
     scanf("%f", &area2);
@@ -75,13 +74,58 @@ int main(){
 
     // Implement of menu interactive
 
-    printf("##### Menu #####\n");
+    printf("##### MENU #####\n");
+    printf("Selecione a opção desejada:\n");
     printf("1. Regras do Game.\n");
     printf("2. Comparar o atributo População\n");
     printf("3. Comparar o atributo Área\n");
     printf("4. Comparar o atributo PIB\n");
     printf("5. Comparar o atributo Ponto Turístico\n");
     printf("6. Comparar o atributo Densidade Populacional\n");
+
+    printf("Digite a população: ");
+    scanf("%d", &option);
+    getchar();
+
+    switch (option)
+    {
+    case 1:
+        // Regras do Game
+        printf("As regras do game são ...\n");
+        break;
+    case 2:
+        // Lógica de comparação da população
+        if (population1 == population2)
+        {
+            printf("População carta1: %d População carta1: %d\n", population1, population2);
+            printf("Empate!");
+        }else if (population1 > population2)
+        {
+            printf("População carta1: %d População carta1: %d\n", population1, population2);
+            printf("Carta 1 Ganhou!\n");
+        }else{
+            printf("População carta1: %d População carta1: %d\n", population1, population2);
+            printf("carta 2 Ganhou!");
+        }
+        
+        break;
+    case 3:
+        // Lógica de comparação da área
+        break;
+    case 4:
+        // Lógica de comparação do PIB
+        break;
+    case 5:
+        // Lógica de comparação de Ponto Turístico
+        break;
+    case 6:
+        // Lógica de comparação da Densidade Populacional
+        break;
+    
+    default:
+        printf("*** OPÇÃO INVALIDA!!! ***");
+        break;
+    }
 
 
 
