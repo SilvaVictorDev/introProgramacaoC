7ea5cc8053e1cd4666494fea2719df5759c17d64
SilvaVictorDev <silva_victor@id.uff.br>
2025-10-20 09:02:46 -0300

    novos arquivos

--- notasMarizes.c
+++ notasMarizes.c
@@ -4,20 +4,20 @@ int main(){
 
     int index;
 
-    char * nomesAlunos = {
+    const char * nomesAlunos[3][3] = {
         {"Aluno 0", "Pt: 30", "Mat: 90"},
         {"Aluno 1", "Pt: 60", "Mat: 60"},
         {"Aluno 2", "Pt: 90", "Mat: 30"}
     };
 
-    printf("digite o número do aluno que queira ver as notas... \n");
+    printf("Digite o número do aluno que queira ver as notas... \n");
     printf("0 - Para aluno 0 \n");
     printf("1 - Para aluno 1 \n");
     printf("2 - Para aluno 2 \n");
 
     scanf("%d", &index);
 
-    printf("a nota do %s são: %s, %s... \n");
+    printf("A nota do %s são: %s, %s... \n",nomesAlunos[index][0], nomesAlunos[index][1], nomesAlunos[index][2]);
     
 
     return 0;
